73bc2eee2eef79656f98e98fd7441da0d7dbe0e4
Carsem <Kennedyc.1999@gmail.com>
2019-11-07 10:19:58 -0800

    Completed check rank feature

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from riot_api import *
 # Your own discord bot token goes here
 Token = open("token.txt", "r").read().rstrip()
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='$')
 
 
 @client.event
@@ -55,20 +55,76 @@ async def _8ball(ctx, *, question):
     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
-@client.command(aliases=['lookup', 'search', 'rank'])
-async def rank_lookup(ctx, summoner_name):
-    d = User({summoner_name})
+@client.command(aliases=['lookup'])
+async def rank_lookup(ctx, *, summoner_name):
+    d = User(summoner_name)
+    d.check_solo_rank()
+    d.check_tft_rank()
+    d.check_flex_rank()
+    if ' ' in d.name:
+        url_name = d.name.replace(' ', '+')
+        embed = discord.Embed(title=f'{d.name}', url="https://na.op.gg/summoner/userName={}".format(url_name), color=0x2e64ec)
+        embed.set_author(name=f"{summoner_name}", url=f'https://na.op.gg/summoner/userName={url_name}')
+    else:
+        embed = discord.Embed(title=f'{d.name}', url="https://na.op.gg/summoner/userName={}".format(d.name), color=0x2e64ec)
+        embed.set_author(name=f"{summoner_name}", url=f'https://na.op.gg/summoner/userName={d.name}')
 
-    # Final look of embed for .lookup
-    embed = discord.Embed(title= f'{summoner_name}', url='https://na.op.gg/summoner/userName={summoner_name}', color=0x400080)
-    embed.set_author(name=f"{summoner_name}", url='https://na.op.gg/summoner/userName={summoner_name}', icon_url="https://i.pinimg.com/originals/69/61/ab/6961ab1af799f02df28fa74278d78120.png%22")
-    embed.set_thumbnail(url="https://i.pinimg.com/originals/69/61/ab/6961ab1af799f02df28fa74278d78120.png%22")
-    embed.add_field(name= "Ranked Solo Duo", value="Rank: Master", inline=True)
-    embed.add_field(name="Wins/Losses", value="256 Wins/ 456 Losses", inline=True)
-    embed.add_field(name="TFT Ranked Play", value="Rank: Challenger", inline=True)
-    embed.add_field(name="Wins/Losses", value="12 wins/ 118 losses", inline=True)
-    embed.add_field(name="Flex 5v5", value="Rank: Gold 5", inline=True)
-    embed.add_field(name="Wins/Losses", value="12 wins/18 Losses", inline=True)
+    if d.solo_dict.get('solo') is None and d.tft_dict.get('TFT') is None and d.sr_dict.get('flex') is None:
+        embed.add_field(name='No Ranked games found', value='Play some ranked...like anything')
+        # else:
+        # pic = d.solo_dict["url"]
+        # embed.add_field(name="Ranked Solo Duo", value=f"Rank: {d.solo_dict['solo']}", inline=True)
+        # embed.add_field(name="Wins/Losses", value=f"{d.solo_dict['wins']} Wins / {d.solo_dict['losses']} Losses", inline=True)
+        # if d.check_tft_rank() is None:
+    elif d.solo_dict.get('solo') is not None:
+        pic = d.solo_dict["url"]
+        embed.set_thumbnail(url=pic)
+        embed.add_field(name="Ranked Solo Duo", value=f"Rank: {d.solo_dict['solo']}", inline=True)
+        embed.add_field(name="Wins/Losses", value=f"{d.solo_dict['wins']} Wins / {d.solo_dict['losses']} Losses", inline=True)
+        if d.tft_dict.get('TFT') is not None:
+            embed.add_field(name="TFT Ranked", value=f"Rank: {d.tft_dict['TFT']}", inline=True)
+            embed.add_field(name="Wins/Losses", value=f" {d.tft_dict['wins']} wins/ {d.tft_dict['losses']} Losses",
+                            inline=True)
+            if d.sr_dict.get('flex') is not None:
+                embed.add_field(name="Flex 5v5", value=f"Rank: {d.sr_dict['flex']}", inline=True)
+                embed.add_field(name="Wins/Losses", value=f" {d.sr_dict['wins']} wins/ {d.sr_dict['losses']} Losses",
+                                inline=True)
+            else:
+                pass
+        else:
+            if d.sr_dict.get('flex') is not None:
+                embed.add_field(name="Flex 5v5", value=f"Rank: {d.sr_dict['flex']}", inline=True)
+                embed.add_field(name="Wins/Losses", value=f" {d.sr_dict['wins']} wins/ {d.sr_dict['losses']} Losses",
+                                inline=True)
+            else:
+                pass
+    elif d.tft_dict.get('TFT') is not None:
+        pic = d.tft_dict["url"]
+        embed.set_thumbnail(url=pic)
+        embed.add_field(name="TFT Ranked", value=f"Rank: {d.tft_dict['TFT']}", inline=True)
+        embed.add_field(name="Wins/Losses", value=f" {d.tft_dict['wins']} wins/ {d.tft_dict['losses']} Losses", inline=True)
+        if d.sr_dict.get()['flex'] is not None:
+            embed.add_field(name="Flex 5v5", value=f"Rank: {d.sr_dict['flex']}", inline=True)
+            embed.add_field(name="Wins/Losses", value=f" {d.sr_dict['wins']} wins/ {d.sr_dict['losses']} Losses",
+                             inline=True)
+        else:
+            pass
+    elif d.sr_dict.get('flex') is not None:
+        pic = d.sr_dict["url"]
+        embed.set_thumbnail(url=pic)
+        embed.add_field(name="Flex 5v5", value=f"Rank: {d.sr_dict['flex']}", inline=True)
+        embed.add_field(name="Wins/Losses", value=f" {d.sr_dict['wins']} wins/ {d.sr_dict['losses']} Losses",
+                        inline=True)
+    else:
+        pass
+        # pic = d.solo_dict["url"]
+        # embed.add_field(name="Ranked Solo Duo", value=f"Rank: {d.solo_dict['solo']}", inline=True)
+        # embed.add_field(name="Wins/Losses", value=f"{d.solo_dict['wins']} Wins / {d.solo_dict['losses']} Losses", inline=True)
+        # embed.add_field(name="TFT Ranked", value=f"Rank: {d.tft_dict['TFT']}", inline=True)
+        # embed.add_field(name="Wins/Losses", value=f" {d.tft_dict['wins']} wins/ {d.tft_dict['losses']} Losses", inline=True)
+        # embed.add_field(name="Flex 5v5", value=f"Rank: {d.sr_dict['flex']}", inline=True)
+        # embed.add_field(name="Wins/Losses", value=f" {d.sr_dict['wins']} wins/ {d.sr_dict['losses']} Losses", inline=True)
+
     await ctx.send(embed=embed)
 
 client.run(Token)
--- riot_api.py
+++ riot_api.py
@@ -27,6 +27,9 @@ class User:
 
     def __init__(self, summoner_name):
         self.name = summoner_name
+        self.sr_dict = dict()
+        self.solo_dict = dict()
+        self.tft_dict = dict()
 
     def get_ranked_info(self):
         summoner_id = self.get_summoner_id()
@@ -41,72 +44,226 @@ class User:
         else:
             print(response.status_code)
         list_of_stats = response.json()
-        return print(list_of_stats)
+        return list_of_stats
 
-# TODO: Check rank from json object and return rank
-
-    def check_rank(self):
+    def check_solo_rank(self):
         list_of_stats = self.get_ranked_info()
         for index in list_of_stats:
-            if 'SR' in index['queueType']:
+            if 'SOLO' in index['queueType']:
                 tier = index['tier']
-                if 'BRONZE' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://bronze.png")
+                if 'IRON' in tier:
+                    rank = 'Iron' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/0/03/Season_2019_-_Iron_1.png/revision/latest?cb=20181229234926"
+                    self.solo_dict['url'] = pic
+                elif 'BRONZE' in tier:
+                    rank = 'Bronze' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/f/f4/Season_2019_-_Bronze_1.png/revision/latest?cb=20181229234910"
+                    self.solo_dict['url'] = pic
                 elif 'SILVER' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://silver.png")
+                    rank = 'Silver' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/70/Season_2019_-_Silver_1.png/revision/latest?cb=20181229234936"
+                    self.solo_dict['url'] = pic
                 elif 'GOLD' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://gold.png")
+                    rank = 'Gold' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/9/96/Season_2019_-_Gold_1.png/revision/latest?cb=20181229234920"
+                    self.solo_dict['url'] = pic
                 elif 'PLATINUM' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://platinum.png")
+                    rank = 'Platinum' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/74/Season_2019_-_Platinum_1.png/revision/latest?cb=20181229234932"
+                    self.solo_dict['url'] = pic
                 elif 'DIAMOND' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://diamond.png")
+                    rank = 'Diamond' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/9/91/Season_2019_-_Diamond_1.png/revision/latest?cb=20181229234917"
+                    self.solo_dict['url'] = pic
                 elif 'MASTER' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://master.png")
+                    rank = 'Master' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/1/11/Season_2019_-_Master_1.png/revision/latest?cb=20181229234929"
+                    self.solo_dict['url'] = pic
                 elif 'GRANDMASTER' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://grandmaster.png")
+                    rank = 'Grandmaster' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/76/Season_2019_-_Grandmaster_1.png/revision/latest?cb=20181229234923"
+                    self.solo_dict['url'] = pic
                 elif 'CHALLENGER' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://challenger.png")
-            if 'SR' in index['queueType']:
+                    rank = 'Challenger' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.solo_dict['wins'] = index['wins']
+                    self.solo_dict['losses'] = index['losses']
+                    self.solo_dict['solo'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/5/5f/Season_2019_-_Challenger_1.png/revision/latest?cb=20181229234913"
+                    self.solo_dict['url'] = pic
+                else:
+                    return None
+
+    def check_tft_rank(self):
+        list_of_stats = self.get_ranked_info()
+        for index in list_of_stats:
+            if 'TFT' in index['queueType']:
                 tier = index['tier']
-                if 'BRONZE' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://bronze.png")
+                if 'IRON' in tier:
+                    rank = 'Iron' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/0/03/Season_2019_-_Iron_1.png/revision/latest?cb=20181229234926"
+                    self.tft_dict['url'] = pic
+                elif 'BRONZE' in tier:
+                    rank = 'Bronze' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/f/f4/Season_2019_-_Bronze_1.png/revision/latest?cb=20181229234910"
+                    self.tft_dict['url'] = pic
                 elif 'SILVER' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://silver.png")
+                    rank = 'Silver' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/70/Season_2019_-_Silver_1.png/revision/latest?cb=20181229234936"
+                    self.tft_dict['url'] = pic
                 elif 'GOLD' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://gold.png")
+                    rank = 'Gold' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/9/96/Season_2019_-_Gold_1.png/revision/latest?cb=20181229234920"
+                    self.tft_dict['url'] = pic
                 elif 'PLATINUM' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://platinum.png")
+                    rank = 'Platinum' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/74/Season_2019_-_Platinum_1.png/revision/latest?cb=20181229234932"
+                    self.tft_dict['url'] = pic
                 elif 'DIAMOND' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://diamond.png")
+                    rank = 'Diamond' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/9/91/Season_2019_-_Diamond_1.png/revision/latest?cb=20181229234917"
+                    self.tft_dict['url'] = pic
                 elif 'MASTER' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://master.png")
+                    rank = 'Master' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/1/11/Season_2019_-_Master_1.png/revision/latest?cb=20181229234929"
+                    self.tft_dict['url'] = pic
                 elif 'GRANDMASTER' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://grandmaster.png")
+                    rank = 'Grandmaster' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/76/Season_2019_-_Grandmaster_1.png/revision/latest?cb=20181229234923"
+                    self.tft_dict['url'] = pic
                 elif 'CHALLENGER' in tier:
-                    embed = discord.Embed(title=f'{d.name}', description='Solo/Duo' + {tier}, color=0x00ff00)
-                    embed.set_thumbnail(url="attachment://challenger.png")
+                    rank = 'Challenger' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.tft_dict['wins'] = index['wins']
+                    self.tft_dict['losses'] = index['losses']
+                    self.tft_dict['TFT'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/5/5f/Season_2019_-_Challenger_1.png/revision/latest?cb=20181229234913"
+                    self.tft_dict['url'] = pic
+                else:
+                    return None
+
+    def check_flex_rank(self):
+        list_of_stats = self.get_ranked_info()
+        for index in list_of_stats:
+            if 'FLEX_SR' in index['queueType']:
+                tier = index['tier']
+                if 'IRON' in tier:
+                    rank = 'Iron' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/0/03/Season_2019_-_Iron_1.png/revision/latest?cb=20181229234926"
+                    self.sr_dict['url'] = pic
+                elif 'BRONZE' in tier:
+                    rank = 'Bronze' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/f/f4/Season_2019_-_Bronze_1.png/revision/latest?cb=20181229234910"
+                    self.sr_dict['url'] = pic
+                elif 'SILVER' in tier:
+                    rank = 'Silver' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/70/Season_2019_-_Silver_1.png/revision/latest?cb=20181229234936"
+                    self.sr_dict['url'] = pic
+                elif 'GOLD' in tier:
+                    rank = 'Gold' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/9/96/Season_2019_-_Gold_1.png/revision/latest?cb=20181229234920"
+                    self.sr_dict['url'] = pic
+                elif 'PLATINUM' in tier:
+                    rank = 'Platinum' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/74/Season_2019_-_Platinum_1.png/revision/latest?cb=20181229234932"
+                    self.sr_dict['url'] = pic
+                elif 'DIAMOND' in tier:
+                    rank = 'Diamond' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/9/91/Season_2019_-_Diamond_1.png/revision/latest?cb=20181229234917"
+                    self.sr_dict['url'] = pic
+                elif 'MASTER' in tier:
+                    rank = 'Master' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/1/11/Season_2019_-_Master_1.png/revision/latest?cb=20181229234929"
+                    self.sr_dict['url'] = pic
+                elif 'GRANDMASTER' in tier:
+                    rank = 'Grandmaster' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/7/76/Season_2019_-_Grandmaster_1.png/revision/latest?cb=20181229234923"
+                    self.sr_dict['url'] = pic
+                elif 'CHALLENGER' in tier:
+                    rank = 'Challenger' + ' ' + index['rank'] + ' ' + str(index['leaguePoints']) + ' ' + 'LP'
+                    self.sr_dict['wins'] = index['wins']
+                    self.sr_dict['losses'] = index['losses']
+                    self.sr_dict['flex'] = rank
+                    pic = "https://vignette.wikia.nocookie.net/leagueoflegends/images/5/5f/Season_2019_-_Challenger_1.png/revision/latest?cb=20181229234913"
+                    self.sr_dict['url'] = pic
+                else:
+                    return None
 
 
-# Quick tests
 
-d = User('Ina')
-d.get_ranked_info()
-d.check_rank()
+
+
+
 
 
 
